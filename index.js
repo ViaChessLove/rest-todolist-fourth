b5375c67380bbabcc8a342808952881d103fe3e1
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2023-04-16 21:19:23 +0500

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 
 import todoRouter from './Todo/router.js';
 
@@ -13,6 +15,7 @@ const DB_URL = process.env.DB_URL;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', todoRouter);
 
 (async () => {
